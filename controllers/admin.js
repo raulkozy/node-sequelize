c2cd6d9b89cca9aa7b75dcb06ffa627a44f0338c
rahul <rahul.kohli@accionlabs.com>
2020-04-03 16:25:48 +0530

    The Node-sequelize and Ejs project final

---
+++ controllers/admin.js
@@ -0,0 +1,99 @@
+const Product = require("../models/product");
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  req.user.createProduct(
+    {
+      description: description,
+      title: title,
+      imageUrl: imageUrl,
+      price: price,
+      userId: req.user.id
+    }
+  )
+    .then(result => {
+      console.log("Created Product");
+      res.redirect("/admin/products");
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  const prodId = req.params.productId;
+  req.user.getProducts({where: {id: 1}})
+  // Product.findByPk(prodId)
+    .then(products => {
+      if (!products) {
+        return res.redirect("/");
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: products[0]
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  
+  Product.findByPk(prodId).then(
+    product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDesc;
+      return product.save();
+    }
+  )
+  .then(result => {
+    console.log('UPDATE PRODUCT !')
+    res.redirect("/admin/products");
+  })
+  .catch(error => console.log(error))
+};
+
+exports.getProducts = (req, res, next) => {
+ req.user.getProducts()
+    .then(products => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products"
+      });
+    })
+    .catch(error => console.log(error));
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.destroy({ where: {id: prodId}})
+  .then(result => {
+    console.log('PRODUCT DELETED !')
+    res.redirect("/admin/products");
+  })
+  .catch(error => console.log(error));
+};
